c5f9f6d97b21c5408f7d0ec35ee8027e7c88e6be
shaheenrumi <Shaheenrumi@uconnect.pk>
2019-01-02 10:56:17 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,135 @@
+
+// function showPrimes(n) {
+//     nextPrime: for (let i = 2; i < n; i++) {
+  
+//       for (let j = 2; j < i; j++) {
+//         if (i % j == 0) continue nextPrime;
+//       }
+  
+//       alert( i ); // a prime
+//     }
+//   }
+
+
+//  let x = prompt("x,");
+//  let n = prompt("n,");
+//  let z = x*n;
+
+// var z= function(){
+//     let x = prompt("x", "");
+//     let n = prompt("n","");
+//     let result;
+//     return result=x*n;
+    
+// }
+// alert(z());
+
+// let user ={};
+// //set
+// user["likes bird"]= true;
+
+
+// let key = "likes birds";
+// user[key]= true;
+// // console.log(key); 
+
+// let user ={
+//     name: "shaheen",
+//     age: 30,
+//     address: "astana",
+//     maried: false
+// };
+
+// let value= prompt("what do you want to know about the user","3");
+
+// alert(user[value]);
+
+
+//  let user = {
+//      name: "Jhon",
+//      age: 30,
+//      isAdmin: true
+//  };
+//  for(let key in user){
+//      alert(key);
+//      alert(user[key]);
+//  }
+
+//  let user ={
+//      name: "jhon",
+//      age:   30
+//  };
+
+//  let clone = {}; 
+//  for (let key in user){
+//      clone[key]= user[key];
+
+//     clone.name ="pete";
+//     alert(user.name);
+//  }
+
+//  let user = {name: "Jhon"};
+
+//  let Permission1 = {canView: true};
+//  let permission2 = {canEdit: true};
+
+//  Object.assin(user, permisson1, permission2);
+
+// hello Objects
+
+// Write the code, one line for each action:
+
+// Create an empty object user.
+// Add the property name with the value John.
+// Add the property surname with the value Smith.
+// Change the value of the name to Pete.
+// Remove the property name from the object.
+
+//  let user={
+//      name: "jhon",
+//      surname: "smith",
+
+//  };
+
+//     user.name="pete"
+//     delete user.name;
+//     console.log(user.name);
+
+
+
+  // Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+//   let schedule = {};
+
+//   console.log(object.isEmpty(schedule));
+
+  // three methods for setting the prototype
+
+  // using a constructor function.
+  // using object.create(prototype)
+  // using object.setPrototypeOf(obj,prototype)- ES6
+
+  // let val;
+
+  // const list = document.querySelector('ul.collection');
+  // const listItem = document.querySelector('li.collection-item:first-child');
+
+  // val =listItem;
+  // val =list;
+
+  // // get child nodes
+
+  // val = list.childNodes;
+  // val=list.childNodes[0];
+
+
+  // console.log(val);
+
+
+
+ 
+
+
+
+
+
